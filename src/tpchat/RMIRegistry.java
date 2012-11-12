6f946238aeac517d7d1461664c089d07b5314516
Tony Martin <tony.martin313@gmail.com>
2012-11-12 16:06:07 +0100

    Démarrage projet
    Classes Main, Client et RMIRegistry

---
+++ src/tpchat/RMIRegistry.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpchat;
+
+import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author Tony MARTIN
+ */
+public class RMIRegistry implements Registry{
+
+        @Override
+        public Remote lookup(String string) throws RemoteException, NotBoundException, AccessException {
+                throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void bind(String string, Remote remote) throws RemoteException, AlreadyBoundException, AccessException {
+                throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void unbind(String string) throws RemoteException, NotBoundException, AccessException {
+                throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void rebind(String string, Remote remote) throws RemoteException, AccessException {
+                throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public String[] list() throws RemoteException, AccessException {
+                throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+}
