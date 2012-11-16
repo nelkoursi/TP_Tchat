7bde66ec2ddd2fe8a83659c5c3b5428124fe8ef9
Tony Martin <tony.martin313@gmail.com>
2012-11-16 10:44:28 +0100

    formatage

--- src/src/tpchat/InterfaceRMIChat.java
+++ src/src/tpchat/InterfaceRMIChat.java
@@ -1,6 +1,7 @@
 package src.tpchat;
 
 public interface InterfaceRMIChat extends java.rmi.Remote {
-	public void sayHello() 
-	throws java.rmi.RemoteException;
+
+        public void sayHello()
+                throws java.rmi.RemoteException;
 }
