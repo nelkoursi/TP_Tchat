f227a0463fc06cda8ea2961ec49f209d99b370ae
AmandineLavergne <lavergne.amandine@gmail.com>
2012-11-16 16:13:05 +0100

    Complément du client (méthodes getteurs et setteurs usuelles)

--- tpchat/ClientServeur.java
+++ tpchat/ClientServeur.java
@@ -1,7 +1,40 @@
 package tpchat;
-
+import java.io.Serializable;
 import java.rmi.*;
-public class ClientServeur {
+
+/**
+ *
+ * @author Gaetan ARRONDEAU, Nora EL KOURSI, Amandine LAVERGNE, Tony MARTIN
+ */
+public class ClientServeur  implements Serializable  {
+	protected String id;
+	protected boolean estConnecte = 0;
+	protected InterfaceRMIChat interfaceChat;
+	
+	//getteur et setteur de l'identifiant du client
+	public String getID() {
+		return id;
+	}
+	public void setID(String iD) {
+		this.id = iD;
+	}
+	
+	//getteur et setteur de l'interface du serveur du chat
+	public InterfaceRMIChat getInterfaceChat() {
+		return interfaceChat;
+	}
+	public void setServChat(InterfaceRMIChat servChat) {
+		this.interfaceChat = interfaceChat;
+	}
+    
+	//getteur et setteur de estConnecté
+	public boolean getEstConnecte() {
+		return estConnecte;
+	}
+	public void setEstConnecte(boolean estConnecte) {
+		this.estConnecte = estConnecte;
+	}
+	
 	public static void main(String args[]) {
 		try {
 			// Récupération d'un stub sur l'objet serveur.
@@ -12,4 +45,7 @@ public class ClientServeur {
 		catch (Exception exc) {
 		}
   }
+
+	
+	
 }
